8ea5a51b9d9fea2dad64d3eddd16c0e28680ac9e
Jeff Greco <jeffpgreco@gmail.com>
2019-10-16 15:46:18 -0400

    got everything working but modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,44 +1,8 @@
-var friends = [
-    {
-        "name": "Tommy",
-        "photo": "../../assets/images/tommy.jpg",
-        "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    },
-    {
-        "name": "Claudette",
-        "photo": "../../assets/images/claudette.jpg",
-        "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    },
-    {
-        "name": "Denny",
-        "photo": "../../assets/images/denny.jpg",
-        "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    },
-    {
-        "name": "Mark",
-        "photo": "../../assets/images/mark.jpg",
-        "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    },
-    {
-        "name": "Mike",
-        "photo": "../../assets/images/mike.jpg",
-        "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    },
-    {
-        "name": "Lisa",
-        "photo": "../../assets/images/lisa.jpg",
-        "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    },
-    {
-        "name": "Flower Bruh",
-        "photo": "../../assets/images/flower.jpg",
-        "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    },
-    {
-        "name": "Doggie",
-        "photo": "../../assets/images/doggie.jpg",
-        "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    }
-];
+
+var fs = require("fs");
+var path = require("path");
+
+var data = fs.readFileSync(path.join(__dirname, "friends.json"));
+var friends = JSON.parse(data);
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,6 +11,43 @@ module.exports = function apiDirect(app) {
     });
 
     app.post("/api/friends", function (req, res) {
-       
+
+        var newArray = [];
+        var difference;
+        var newFriend = req.body;
+
+        for (var i = 0; i < friends.length; i++) {
+
+            difference = 0;
+
+            for (var j = 0; j < newFriend.scores.length; j++) {
+
+                difference += Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+
+            };
+
+            newArray.push(difference);
+
+        };
+
+        var match = newArray.indexOf(Math.min(...newArray));
+
+        friends.push(newFriend);
+
+        fs.readFile(path.join(__dirname, "../data/friends.json"), "utf8", function (err, data) {
+            if (err) throw err;
+
+            var jsonParsed = JSON.parse(data);
+
+            jsonParsed.push(newFriend);
+            
+            fs.writeFile(path.join(__dirname, "../data/friends.json"), JSON.stringify(jsonParsed, null, 2), function (err) {
+                if (err) throw err;
+            });
+
+        });
+
+        res.json(friends[match]);
+
     });
 }

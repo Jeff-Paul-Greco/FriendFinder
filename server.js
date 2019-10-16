95593e117bd338c557f74ef189c03fc94fa1d78c
Jeff Greco <jeffpgreco@gmail.com>
2019-10-16 04:46:33 -0400

    setup server js

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-var path = require("path");
+
 var express = require("express");
+var path  = require("path");
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/app/public")));
+
+app.listen(PORT, function() {
+    console.log("Application listening on PORT " + PORT);
+});
